5f9776a91c0d861f91a114d6bd6165a25ff1b875
partharoyy <69928055+partharoyy@users.noreply.github.com>
2024-06-08 23:25:37 +0530

    Few refactor

--- src/topics/currency-converter/CurrencyConverter.jsx
+++ src/topics/currency-converter/CurrencyConverter.jsx
@@ -26,34 +26,36 @@ function CurrencyConverter() {
   }, [amount, fromCurrency, toCurrency]);
 
   return (
-    <div className='currency-main-container'>
+    <div>
       <h1>Currency Converter</h1>
-      <div className='from'>
-        <input
-          type='number'
-          onChange={(e) => setAmount(e.target.value)}
-          value={amount}
-          name='amount'
-          placeholder='Enter a amount'
-        />
-        <select value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
-          <option value='USD'>USD</option>
-          <option value='INR'>INR</option>
-          <option value='EUR'>EUR</option>
-        </select>
+      <div className='currency-main-container'>
+        <div className='from'>
+          <input
+            type='number'
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+            name='amount'
+            placeholder='Enter a amount'
+          />
+          <select value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
+            <option value='USD'>USD</option>
+            <option value='INR'>INR</option>
+            <option value='EUR'>EUR</option>
+          </select>
+        </div>
+        <p>To</p>
+        <div className='to'>
+          <input type='text' readOnly value={convertedAmount} />
+          <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
+            <option value='USD'>USD</option>
+            <option value='INR'>INR</option>
+            <option value='EUR'>EUR</option>
+          </select>
+        </div>
+        <p className='currency-info'>
+          Exchange Rate: 1 {fromCurrency} = {exchangeRate} {toCurrency}
+        </p>
       </div>
-      <p>To</p>
-      <div className='to'>
-        <input type='text' readOnly value={convertedAmount} />
-        <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
-          <option value='USD'>USD</option>
-          <option value='INR'>INR</option>
-          <option value='EUR'>EUR</option>
-        </select>
-      </div>
-      <p className='currency-info'>
-        Exchange Rate: 1 {fromCurrency} = {exchangeRate} {toCurrency}
-      </p>
     </div>
   );
 }
