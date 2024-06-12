547bdc388c15400fe133efe7ad47c6ce4974771e
partharoyy <69928055+partharoyy@users.noreply.github.com>
2024-06-12 21:55:15 +0530

    Fix errmessage and result info display

--- src/topics/tip-calculator/tipCalculator.jsx
+++ src/topics/tip-calculator/tipCalculator.jsx
@@ -6,12 +6,13 @@ function TipCalculator() {
   const [billAmount, setBillAmount] = useState(null);
   const [tipPercentage, setTipPercentage] = useState(10);
   const [splitCount, setSplitCount] = useState(1);
-  const [tipAmount, setTipAmount] = useState(0);
+  const [tipAmount, setTipAmount] = useState(null);
   const [errorMsg, setErrorMsg] = useState('');
 
   function handleCalculateTip() {
     if (!billAmount || billAmount <= 0 || !tipPercentage || tipPercentage <= 0) {
       setErrorMsg('Enter valid bill and tip amount');
+      setTipAmount(null);
       return;
     }
 
@@ -26,32 +27,35 @@ function TipCalculator() {
       tipPerPerson: tipAmountPerPerson.toFixed(2),
       totalPerPerson: totalAmountPerPerson.toFixed(2),
     });
+    setErrorMsg('');
   }
 
   return (
     <div>
       <h1>Tip calculator</h1>
-      <div>
-        <label>Bill Amount</label>
-        <input type='number' value={billAmount} onChange={(e) => setBillAmount(e.target.value)} />
-      </div>
-      <div>
-        <label>Tip Percentage</label>
-        <input type='number' value={tipPercentage} onChange={(e) => setTipPercentage(e.target.value)} />
-      </div>
-      <div>
-        <label>Number of people</label>
-        <input type='number' value={splitCount} onChange={(e) => setSplitCount(e.target.value)} />
-      </div>
-      <button onClick={handleCalculateTip}>Calculate Tip</button>
-      {tipAmount ? (
+      <div className='tip-main-container'>
         <div>
-          <p>Total Amount: {tipAmount.totalAmount} </p>
-          <p>Tip per person: {tipAmount.tipPerPerson}</p>
-          <p>Total amount per person: {tipAmount.totalPerPerson}</p>
+          <label>Bill Amount</label>
+          <input type='number' value={billAmount} onChange={(e) => setBillAmount(e.target.value)} />
         </div>
-      ) : null}
-      {errorMsg && <p>{errorMsg}</p>}
+        <div>
+          <label>Tip Percentage</label>
+          <input type='number' value={tipPercentage} onChange={(e) => setTipPercentage(e.target.value)} />
+        </div>
+        <div>
+          <label>Number of people</label>
+          <input type='number' value={splitCount} onChange={(e) => setSplitCount(e.target.value)} />
+        </div>
+        <button onClick={handleCalculateTip}>Calculate Tip</button>
+        {tipAmount ? (
+          <div>
+            <p>Total Amount: {tipAmount.totalAmount} </p>
+            <p>Tip per person: {tipAmount.tipPerPerson}</p>
+            <p>Total amount per person: {tipAmount.totalPerPerson}</p>
+          </div>
+        ) : null}
+        {errorMsg && <p>{errorMsg}</p>}
+      </div>
     </div>
   );
 }
