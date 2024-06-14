879ad96c38394e5e3006ea6983ca6f1b8a4e34c6
partharoyy <69928055+partharoyy@users.noreply.github.com>
2024-06-14 22:57:46 +0530

    Add logic

--- src/topics/music-player/MusicPlayer.jsx
+++ src/topics/music-player/MusicPlayer.jsx
@@ -1,9 +1,69 @@
+import { useEffect, useRef, useState } from 'react';
 import './MusicPlayer.css';
 
+const tracks = [
+  {
+    title: 'Track 1',
+    source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    title: 'Track 2',
+    source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+    image: 'https://via.placeholder.com/150',
+  },
+];
+
 function MusicPlayer() {
+  const [currentSong, setCurrentSong] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songProgress, setSongProgress] = useState(0);
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSongProgress((audioRef.current.currentTime / audioRef.current.duration) * 100);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isPlaying]);
+
+  function handlePlayPause() {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+
+    setIsPlaying((prev) => !prev);
+  }
+
+  function handleSkipSong(direction) {
+    if (direction === 'backward') {
+      setCurrentSong((prevTrack) => (prevTrack + 1) % tracks.length);
+    } else if (direction === 'forward') {
+      setCurrentSong((prevTrack) => (prevTrack - 1 + tracks.length) % tracks.length);
+    }
+
+    setSongProgress(0);
+  }
+
   return (
     <div>
       <h1>Music Player</h1>
+      <div>{tracks[currentSong].title}</div>
+      <img src={tracks[currentSong].image} alt={tracks[currentSong].title} />
+      <audio ref={audioRef} src={tracks[currentSong].source} />
+      <div className='music-progress'>
+        <div style={{ width: `${songProgress}%`, background: isPlaying ? '#3498db' : '#a43636', height: '15px' }}></div>
+      </div>
+      <div>
+        <button onClick={() => handleSkipSong('backward')}>Backward</button>
+        <button onClick={handlePlayPause}>Play/Pause</button>
+        <button onClick={() => handleSkipSong('forward')}>Forward</button>
+      </div>
     </div>
   );
 }
